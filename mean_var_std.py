7070a537e95b20cbed669738a7f91fdd31ddd0d6
Azadeh Kooshesh <azadeh.kooshesh@uni-rostock.de>
2022-09-07 18:30:03 +0200

    first commit

---
+++ mean_var_std.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+def calculate(list):
+  if len(list) != 9:
+    raise ValueError("Please enter 9 value")
+  array = np.array(list).reshape((3, 3))
+  calculations = {
+    "mean": [
+      np.mean(array, axis = 0).tolist(),
+      np.mean(array, axis = 1).tolist(),
+      np.mean(array.tolist())
+    ],
+    
+    "variance": [
+      np.var(array, axis = 0).tolist(),
+      np.var(array, axis = 1).tolist(),
+      np.var(array) .tolist()    
+    ],
+    
+    "standard deviation": [
+      np.std(array, axis = 0).tolist(),
+      np.std(array, axis = 1).tolist(),
+      np.std(array).tolist()     
+    ],    
+  }
+
+  return calculations
